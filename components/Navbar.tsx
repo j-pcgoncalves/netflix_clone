f72cd38e2b9807a677370326e31a5913305ce8e5
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-30 11:50:59 +0000

    create MobileMenu component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { BellIcon, MagnifyingGlassIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 
-import NavbarItem from "./NavbarItem";
+import MobileMenu from "@/components/MobileMenu";
+import NavbarItem from "@/components/NavbarItem";
 
 const TOP_OFFSET = 66;
 
 const Navbar = () => {
+    const [showAccountMenu, setShowAccountMenu] = useState(false);
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
     const [showBackground, setShowBackground] = useState(false);
 
     useEffect(() => {
@@ -24,6 +27,14 @@ const Navbar = () => {
         }
     }, []);
 
+    const toggleAccountMenu = useCallback(() => {
+        setShowAccountMenu(current => !current);
+    }, []);
+
+    const toggleMobileMenu = useCallback(() => {
+        setShowMobileMenu(current => !current);
+    }, []);
+
     return (
         <nav className="w-full fixed z-40">
             <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""}`}>
@@ -35,6 +46,11 @@ const Navbar = () => {
                     <NavbarItem label="New & Popular" />
                     <NavbarItem label="My List" />
                     <NavbarItem label="Browse by Languages" />
+                </div>
+                <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+                    <p className="text-white text-sm">Browse</p>
+                    <ChevronDownIcon className={`w-4 text-white fill-white transition ${showMobileMenu ? "rotate-180" : "rotate-0"}`} />
+                    <MobileMenu visible={showMobileMenu} />
                 </div>
             </div>
         </nav>

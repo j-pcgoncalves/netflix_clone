707704cd0fd03a881e613f3354eb1cc02ea7b8b4
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-11-30 11:26:44 +0000

    create InfoModal and effects

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -15,9 +15,30 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
     const { movieId } = useInfoModalStore();
     const { data = {} } = useMovie(movieId);
 
+    useEffect(() => {
+        setIsVisible(!!visible);
+    }, [visible]);
+
+    const handleClose = useCallback(() => {
+        setIsVisible(false);
+        setTimeout(() => {
+            onClose();
+        }, 300);
+    }, [onClose]);
+
+    if (!visible) {
+        return null;
+    }
+
     return (
         <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
-
+            <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
+                <div className={`${isVisible ? "scale-100" : "scale-0"} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
+                    <div className="relatve h-96">
+                        <video poster={data?.thumbnailUrl} autoPlay muted loop src={data?.videoUrl} className="w-full brightness-[60%] object-cover h-full" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

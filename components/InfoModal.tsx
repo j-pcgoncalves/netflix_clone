91e51faa6c8d2ddc894736ecf10abc701b35df80
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-01 17:29:35 +0000

    create favorite button and functionality

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 
-import useMovie from "@/hooks/useMovie";
 import useInfoModalStore from "@/hooks/useInfoModalStore";
+import useMovie from "@/hooks/useMovie";
+import PlayButton from "@/components/PlayButton";
+import FavoriteButton from "@/components/FavoriteButton";
 
 interface InfoModalProps {
     visible?: boolean;
@@ -36,6 +38,18 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
                 <div className={`${isVisible ? "scale-100" : "scale-0"} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
                     <div className="relatve h-96">
                         <video poster={data?.thumbnailUrl} autoPlay muted loop src={data?.videoUrl} className="w-full brightness-[60%] object-cover h-full" />
+                        <div onClick={handleClose} className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center">
+                            <XMarkIcon className="text-white w-6" />
+                        </div>
+                        <div className="absolute bottom-[10%] left-10">
+                            <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
+                                {data?.title}
+                            </p>
+                            <div className="flex flex-row gap-4 items-center">
+                                <PlayButton movieId={data?.id} />
+                                <FavoriteButton movieId={data?.id} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

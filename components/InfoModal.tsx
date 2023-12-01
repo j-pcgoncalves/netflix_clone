549deebc62fa74849877db2b0f97e1c863850f76
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-01 17:35:26 +0000

    add info to info modal

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -41,7 +41,7 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
                         <div onClick={handleClose} className="cursor-pointer absolute top-3 right-3 h-10 w-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center">
                             <XMarkIcon className="text-white w-6" />
                         </div>
-                        <div className="absolute bottom-[10%] left-10">
+                        <div className="absolute bottom-[40%] left-10">
                             <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
                                 {data?.title}
                             </p>
@@ -51,10 +51,28 @@ const InfoModal: React.FC<InfoModalProps> = ({ visible, onClose }) => {
                             </div>
                         </div>
                     </div>
+
+                    <div className="px-12 py-8">
+                        <div className="flex flex-row items-center gap-2 mb-8">
+                            <p className="text-green-400 font-semibold text-lg">
+                                New
+                            </p>
+                            <p className="text-white text-lg">
+                                {data?.duration}
+                            </p>
+                            <p className="text-white text-lg">
+                                {data?.genre}
+                            </p>
+                        </div>
+                        <p className="text-white text-lg">
+                            {data?.description}
+                        </p>
+                    </div>
+
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default InfoModal;

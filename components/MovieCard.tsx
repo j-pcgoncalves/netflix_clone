98bcbc749256389d9d063de1ab531c8d2bfbf0ee
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-12-01 23:05:51 +0000

    create MovieCard component

---
+++ components/MovieCard.tsx
@@ -0,0 +1,15 @@
+import React, { useCallback } from "react";
+
+import { MovieInterface } from "@/types";
+
+interface MovieCardProps {
+    data: MovieInterface;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+    return (
+        <div></div>
+    )
+}
+
+export default MovieCard;
